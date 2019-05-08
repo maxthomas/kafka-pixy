0484c55ac92f5ae62aa5f903ff4fb49830d0c62f
max thomas <max@maxthomas.io>
2019-05-08 13:41:46 -0400

    try to work in go module in travis CI

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -13,7 +13,7 @@ go fmt $PKGS
 go vet $PKGS
 
 echo "Run tests for all packages and aggregate coverage data to `cover.out`"
-go test -v -p 1 --parallel=1 -timeout=600s -covermode=atomic -coverprofile=cover.out -coverpkg $PKGS_DELIM ./... -check.vv;
+env GO111MODULE=on go test -v -p 1 --parallel=1 -timeout=600s -covermode=atomic -coverprofile=cover.out -coverpkg $PKGS_DELIM ./... -check.vv;
 
 echo "Ship the coverage report to coveralls.io service"
 go get github.com/mattn/goveralls
